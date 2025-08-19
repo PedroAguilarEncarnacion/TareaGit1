19720d84a00ab2d7b72a4f95e5f352f8dfd1f63a
Pedro Aguilar Encarnación <pedro_stalyn@hotmail.com>
2025-08-18 19:23:25 -0500

    Actualice el mensaje

--- src/main/java/com/mycompany/tarea1ejemplo/Tarea1Ejemplo.java
+++ src/main/java/com/mycompany/tarea1ejemplo/Tarea1Ejemplo.java
@@ -11,6 +11,6 @@ package com.mycompany.tarea1ejemplo;
 public class Tarea1Ejemplo {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("Hola Pedro");
     }
 }
