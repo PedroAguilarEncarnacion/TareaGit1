16ccec07332a7a127dc086c8b7fe723ee11c2676
Pedro Aguilar Encarnación <pedro_stalyn@hotmail.com>
2025-08-18 19:20:54 -0500

    Creación del proyecto

---
+++ src/main/java/com/mycompany/tarea1ejemplo/Tarea1Ejemplo.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.tarea1ejemplo;
+
+/**
+ *
+ * @author User
+ */
+public class Tarea1Ejemplo {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
